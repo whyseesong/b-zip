476348bf411c63d30bf70e111fb2f56af20e4c26
SongYoungCheol <yc@sk.com>
2020-08-19 23:08:30 +0900

    react 컴포넌트 렌더링, css, image import 테스트

---
+++ b-zip-front/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Marker from "@components/Marker";
+
+const App = () => {
+    return (
+        <Marker/>
+    )
+};
+
+export default App;
--- b-zip-front/src/index.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-
-const div = document.querySelector('.container');
-div.innerText = 'bonjour!';
-console.log("123123");
