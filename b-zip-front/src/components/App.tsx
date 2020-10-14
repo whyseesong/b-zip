93964db9d8e752fa4b9412d0618b2ed866b57623
SongYoungCheol <yc@sk.com>
2020-10-14 16:30:46 +0900

    feat: 카카오 맵 추가
    카카오맵을 추가하고 api 키값을 /b-zip/.env에 저장하도록 설정. gitignore에 추가

--- b-zip-front/src/components/App.tsx
+++ b-zip-front/src/components/App.tsx
@@ -1,17 +1,30 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 
 import './default.css';
 
-import Marker from '@components/Marker';
-import StationCard from '@components/StationCard';
 import HeaderContainer from '@containers/HeaderContainer';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 const App = (): ReactElement => {
+  useEffect(() => {
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+    const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  }, []);
+
   return (
     <div>
       <HeaderContainer />
-      <Marker />
-      <StationCard />
+      <div id="map" style={{ width: '100vw', height: '100vh' }} />
     </div>
   );
 };
